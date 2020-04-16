4ddccbeee63dccffaab808f261bbb85d9d941f22
Somnath <somnath@Somnaths-MacBook-Air.local>
2020-04-16 19:14:01 +0530

    Updates

---
+++ precache-manifest.4cc414b2c640e7d46a89217f72021b59.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae02f6204dc24c46f42c8d936a907598",
+    "url": "/Crio-Launch-Feb-2020-adityasrivastava301199/index.html"
+  },
+  {
+    "revision": "27483da616306d5d0d15",
+    "url": "/Crio-Launch-Feb-2020-adityasrivastava301199/static/css/2.57ddf7ad.chunk.css"
+  },
+  {
+    "revision": "2f6d0e898d3f7c006c44",
+    "url": "/Crio-Launch-Feb-2020-adityasrivastava301199/static/css/main.5db1380d.chunk.css"
+  },
+  {
+    "revision": "27483da616306d5d0d15",
+    "url": "/Crio-Launch-Feb-2020-adityasrivastava301199/static/js/2.c8260e32.chunk.js"
+  },
+  {
+    "revision": "d466ab9648f013dea081956fc7c904f3",
+    "url": "/Crio-Launch-Feb-2020-adityasrivastava301199/static/js/2.c8260e32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f6d0e898d3f7c006c44",
+    "url": "/Crio-Launch-Feb-2020-adityasrivastava301199/static/js/main.ba4deb5f.chunk.js"
+  },
+  {
+    "revision": "9b30e6936111e309297b",
+    "url": "/Crio-Launch-Feb-2020-adityasrivastava301199/static/js/runtime-main.4c09fc2d.js"
+  },
+  {
+    "revision": "c9536e0497f939475da3110091e1a10d",
+    "url": "/Crio-Launch-Feb-2020-adityasrivastava301199/static/media/crio-logo-white.c9536e04.svg"
+  },
+  {
+    "revision": "63be71d01edef2dcf3cafba5e52553a4",
+    "url": "/Crio-Launch-Feb-2020-adityasrivastava301199/static/media/crio-logo.63be71d0.svg"
+  }
+]);
